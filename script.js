38152816eed8c6da2fd9172aadb594abeaf85b20
Valentina Garavaglia <vaahxx@gmail.com>
2019-07-15 14:59:02 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let container = document.querySelector(".container");
+let square = document.querySelectorAll(".square");
+
+container.onmousemove() = function valentina() {
+    console.log("xablau");
+}
