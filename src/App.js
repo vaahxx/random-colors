7a593c5ab4272e913ef55df391dfca16b50c07d6
vaahxx <valentina.souza@mercadolivre.com>
2021-09-05 23:16:49 -0300

    update project to use React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ColorfulSquare from "./Square";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Random Colors Square</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <strong>Move your mouse across the squares</strong>
       </header>
+      <div className="App-squares-container">
+        <ColorfulSquare />
+        <ColorfulSquare />
+        <ColorfulSquare />
+        <ColorfulSquare />
+        <ColorfulSquare />
+      </div>
     </div>
   );
 }
---
+++ src/Square.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import "./Square.css";
+
+const getRandomNumber = () => Math.floor(Math.random() * (255 - 0));
+
+export default function ColorfulSquare() {
+  const [backgroundColor, setBackgroundColor] = useState("black");
+  const [width, setWidth] = useState("150px");
+  const [heigth, setHeigth] = useState("150px");
+
+  const handleMouseMove = () => {
+    setBackgroundColor(
+      `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()}`
+    );
+    setWidth(`${getRandomNumber()}px`);
+    setHeigth(`${getRandomNumber()}px`);
+  };
+
+  return (
+    <div
+      className="Colorful-square"
+      style={{ backgroundColor, width, heigth }}
+      onMouseMove={handleMouseMove}
+    ></div>
+  );
+}
