ae488e5deb5a5dbc51c1d37300aae9f3b6e166b1
Valentina Garavaglia <valentina@Valentinas-MacBook-Pro.local>
2022-06-01 18:27:08 -0400

    add transparency to box shadow and change its color

--- src/Square.js
+++ src/Square.js
@@ -4,22 +4,28 @@ import "./Square.css";
 const getRandomNumber = () => Math.floor(Math.random() * (255 - 0));
 
 export default function ColorfulSquare() {
-  const [backgroundColor, setBackgroundColor] = useState("black");
-  const [width, setWidth] = useState("150px");
-  const [heigth, setHeigth] = useState("150px");
+  const [color, setColor] = useState("black");
+  const [boxShadowColor, setBoxShadowColor] = useState("black");
 
   const handleMouseMove = () => {
-    setBackgroundColor(
-      `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()}`
+    const red = getRandomNumber()
+    const green = getRandomNumber()
+    const blue = getRandomNumber()
+    setColor(
+      `rgb(${red}, ${green}, ${blue}`
     );
-    // setWidth(`${getRandomNumber()}px`);
-    // setHeigth(`${getRandomNumber()}px`);
+    setBoxShadowColor(
+      `rgb(${red}, ${green}, ${blue}, 0.6`
+    );
   };
 
   return (
     <div
       className="Colorful-square"
-      style={{ backgroundColor, width, heigth }}
+      style={{ 
+        backgroundColor: color,
+        boxShadow: `10px 10px ${boxShadowColor}`
+      }}
       onMouseMove={handleMouseMove}
     ></div>
   );
